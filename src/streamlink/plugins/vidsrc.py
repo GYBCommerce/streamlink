f7ed803694e91ed3e7a4c3bdb9f1292b97f6d983
milyasyousuf <m_ilyas@outlook.com>
2023-07-15 19:08:18 +0500

    new code updated

--- src/streamlink/plugins/vidsrc.py
+++ src/streamlink/plugins/vidsrc.py
@@ -5,8 +5,10 @@ from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.dash import DASHStream
 from streamlink.stream.hls import HLSStream
+import requests
+from bs4 import BeautifulSoup
 
-#https://vidsrc.me/embed/tt0396269/
+# https://vidsrc.me/embed/tt0396269/
 
 @pluginmatcher(
     name="default",
@@ -20,31 +22,18 @@ class VidSrc(Plugin):
         return cls.url_re.match(url) is not None
 
     def _get_streams(self):
-        #res = self.session.http.get(self.url)
-        # print(res)
-        # m = self.url_re.match(res.url)
-        # video_id = m.group("video_id")
-        # api_url = f"https://vidsrc.me/embed/tt0396269/"
-        headers = {
-            "Referer": self.url,
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
-        }
-        data = self.session.http.get(self.url, headers=headers)
-        print(data.text)
-        # streams = validate(data, validate.Schema({
-        #     "success": bool,
-        #     "result": [{
-        #         "type": str,
-        #         "label": str,
-        #         "file": validate.url(scheme="http"),
-        #         validate.optional("format"): str,
-        #         validate.optional("height"): int,
-        #         validate.optional("width"): int
-        #     }]
-        # }))
-        # for stream in streams["result"]:
-        #     if stream["type"] == "hls":
-        #         yield stream["label"], HLSStream(self.session, stream["file"])
+        response = requests.get(self.url)
+        html_content = response.text
+        soup = BeautifulSoup(html_content, "html.parser")
+        iframe = soup.find("iframe")
+
+        hls_url = f"https:{iframe.get('src')}"
+
+        #return self.session.streams(self.url)
+        return {"live": HLSStream(self.session, hls_url)}
+        #return HLSStream.parse_variant_playlist(self.session, hls_url, headers={"Referer": self.url})
+
+
 # Register the plugin
 #streamlink.plugin.register_plugin(VidsrcPlugin)
 __plugin__ = VidSrc
